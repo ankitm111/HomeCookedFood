3df91720f616a15f216775f275d491c98448573e
Ankit Maheshwari <ankitm111@gmail.com>
2017-04-02 19:06:44 -0700

    Working Authentication. It authenticates using username/password or token

--- app.py
+++ app.py
@@ -1,22 +1,76 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, abort, request, g
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/homecookedfood'
+app.config['SECRET_KEY'] = 'AnkitHarshOnkar'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+auth = HTTPBasicAuth()
 
-from models import Users, Tiffins, Comments, Ratings
+#from models import Users, Tiffins, Comments, Ratings
+# TODO: Fix CICRULAR IMPORTS
+import models
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@auth.verify_password
+def verify_password(email_id_or_token, password):
+    # first try to authenticate by token
+    user = models.Users.verify_auth_token(email_id_or_token)
+    if not user:
+        # try to authenticate with user_id/password
+        user = models.Users.query.filter_by(email_id=email_id_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/homecookedfood/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')}) 
+
+
+@app.route('/homecookedfood/adduser', methods=['POST'])
+def adduser():
+    content = request.json
+    if (not content or not 'name' in content or not 'email_id' in content
+        or not 'password' in content or not 'phone' in content or
+        not 'zipcode' in content):
+        abort(400)
+
+    user = models.Users.query.filter_by(email_id=content['email_id']).first()
+    if user is not None:
+        # existing user
+        abort(400)
+
+    new_user = models.Users(content['name'], content['email_id'], content.get('phone'), content.get('zipcode'))
+    new_user.hash_password(content['password'])
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'id' : new_user.user_id}), 201
 
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,27 +1,59 @@
-from app import db
+import os
+import random, string
+from app import db, app
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 from datetime import datetime
 from sqlalchemy.dialects.postgresql import JSON
+
+
+def randomiamgename(length=20):
+   return ''.join(random.choice(string.lowercase) for i in range(length))
 
 
 class Users(db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
-    email_id = db.Column(db.String(40))
-    password = db.Column(db.String(80))
+    name = db.Column(db.String(40))
+    email_id = db.Column(db.String(40), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     phone_number = db.Column(db.String(11))
     zipcode = db.Column(db.String(10))
-    dishes = db.relationship('Tiffins', backref='users',
-                             lazy='dynamic')
+    tiffins = db.relationship('Tiffins', backref='users',
+                              lazy='dynamic')
 
-    def __init__(self, email_id, password, phone_number, zipcode):
+    def __init__(self, name, email_id, phone_number, zipcode):
+        self.name = name
         self.email_id = email_id
-        self.password = password
         self.phone_number = phone_number
         self.zipcode = zipcode
 
     def __repr__(self):
         return '<id {}>'.format(self.user_id)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration = 60000):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'user_id': self.user_id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = Users.query.filter_by(user_id=data['user_id']).first()
+        return user
 
 
 #Tiffins:  (Drop all the rows with stale Date each day)
