255b1aeaed6e5ab30e5ef1883b4755494f6472d8
Onkar Deshpande <onkar.deshpande@gmail.com>
2017-04-30 20:34:33 -0700

    Update setup script to reflect recent UI setup

--- setup-mac.sh
+++ setup-mac.sh
@@ -13,8 +13,11 @@ if [ ! -d "$POSTGRES_DIR" ]; then
     exit 1
 fi
 
-echo "Creating database homecookedfood..."
-$POSTGRES_BINARY/psql -c "CREATE DATABASE homecookedfood"
+$POSTGRES_BINARY/psql -lqt | cut -d \| -f 1 | grep -qw "homecookedfood"
+if [ $? -ne 0 ]; then
+    echo "Creating database homecookedfood..."
+    $POSTGRES_BINARY/psql -c "CREATE DATABASE homecookedfood"
+fi
 
 if [ ! -x "$VIRTUALENV_BINARY" ]; then
     echo "Installing virtualenv..."
@@ -28,7 +31,7 @@ export PATH="$PATH:$POSTGRES_BINARY"
 echo $PATH
 
 echo "Installing flask and other things..."
-pip install psycopg2 Flask-SQLAlchemy Flask-Migrate flask-httpauth passlib
+pip install psycopg2 Flask-SQLAlchemy Flask-Migrate flask-httpauth passlib gunicorn flask_cors
 
 echo "Exporting flask settings..."
 export APP_SETTINGS=config.DevelopmentConfig
@@ -43,5 +46,9 @@ flask db migrate
 flask db upgrade
 python manage.py db upgrade
 
-echo "Starting flask server..."
-$VIRTUALENV_NAME/bin/flask run
+echo "Starting flask server on localhost:5000"
+$VIRTUALENV_NAME/bin/flask run &
+
+echo "Starting HTTP server on localhost:8080"
+cd UI
+../$VIRTUALENV_NAME/bin/python  -m http.server 8080 &
